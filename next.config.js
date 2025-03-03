c80424e0fd2e5c77e1f6f0babecc362523379474
ishola11 <76501353+ishola11@users.noreply.github.com>
2025-03-03 08:39:55 -0500

    Update the domain to blastcanvas next.config.js

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
       'pickbazarlaravel.s3.ap-southeast-1.amazonaws.com',
       'lh3.googleusercontent.com',
       '127.0.0.1:8000',
-      'api.poducty.com', 
+      'apisoftware.blastcanvas.com', 
     ],
   },
   ...(process.env.APPLICATION_MODE === 'production' && {
